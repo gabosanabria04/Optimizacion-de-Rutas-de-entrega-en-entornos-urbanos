e9d6ce14e26ac487ca9922b5982d4f4b61baa14b
GabrielSanabriaA <gabriel.sanabriaalvarado@ucr.ac.cr>
2025-06-01 15:02:31 -0600

    feat: agregar código a Punto_de_Entrega

--- Punto_de_Entrega.py
+++ Punto_de_Entrega.py
@@ -1,9 +1,34 @@
-from geopy.distance import geodesic
+
 
 class Punto_de_Entrega:
     def __init__(self, latitud, longitud, nombre):
-        self.nombre = nombre 
-        self.latitud = latitud
-        self.longitud = longitud
-        
-        
+        self.__latitud = latitud
+        self.__longitud = longitud
+        self.__nombre = nombre
+
+    @property
+    def latitud(self):
+        return self.__latitud
+
+    @latitud.setter
+    def latitud(self, lat):
+        self.__latitud = lat
+
+    @property
+    def longitud(self):
+        return self.__longitud
+
+    @longitud.setter
+    def longitud(self, long):
+        self.__longitud = long
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, nuevo_nombre):
+        self.__nombre = nuevo_nombre
+
+    def __str__(self):
+        return f"Punto: {self.__nombre} (Latitud: {self.__latitud}, Longitud: {self.__longitud})"
