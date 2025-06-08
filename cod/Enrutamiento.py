5470d78d648fed0eca859d193a595e85ffa21ddc
Paola María Espinoza Hernández <paola.espinozahernandez@ucr.ac.cr>
2025-06-08 17:27:13 -0600

    fix: arregla costos

--- cod/Enrutamiento.py
+++ cod/Enrutamiento.py
@@ -1,7 +1,22 @@
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
 class Enrutamiento():
-    def __init__(self, matriz, depot):
+    def __init__(self, matriz, depot: int):
+        '''
+        Constructor de la clase Enrutamiento
+
+        Parameters
+        ----------
+        matriz : np.ndarray
+            Matriz de distancias entre los lugares.
+        depot : int
+            Índice del lugar de inicio (y fin).
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__data = {
             "distance_matrix": matriz,
             "num_vehicles": 1,
@@ -14,29 +29,109 @@ class Enrutamiento():
 
     @property
     def data(self):
+        '''
+        Getter de los datos para el enrutamiento.
+
+        Returns
+        -------
+        dict
+            Diccionario con la matriz de distancias, el número de vehículos y el lugar de inicio.
+
+        '''
         return self.__data
 
     @data.setter
-    def data(self, new_value):
+    def data(self, new_value: dict):
+        '''
+        Setter de los datos para el enrutamiento.
+
+        Parameters
+        ----------
+        new_value : dict
+            Diccionario con la matriz de distancias, el número de vehículos y el lugar de inicio..
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__data = new_value
 
     @property
     def manager(self):
+        '''
+        Getter del atributo manager.
+
+        Returns
+        -------
+        ortools.constraint_solver.pywrapcp.RoutingIndexManager
+            Manager actual.
+
+        '''
         return self.__manager
 
     @manager.setter
     def manager(self, new_value):
+        '''
+        Setter del atributo manager.
+
+        Parameters
+        ----------
+        new_value : ortools.constraint_solver.pywrapcp.RoutingIndexManager
+            Nuevo manager.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__manager = new_value
 
     @property
     def routing(self):
+        '''
+        Getter del atributo routing.
+
+        Returns
+        -------
+        ortools.constraint_solver.pywrapcp.RoutingModel
+            Routing actual.
+
+        '''
         return self.__routing
 
     @routing.setter
     def routing(self, new_value):
+        '''
+        Setter del atributo routing.
+
+        Parameters
+        ----------
+        new_value : ortools.constraint_solver.pywrapcp.RoutingModel
+            Nuevo routing.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__routing = new_value
 
     def distance_callback(self, from_index, to_index):
+        '''
+        Calcula el costo de ir de un nodo a otro. Los parámetros son propios de ortools.
+
+        Parameters
+        ----------
+        from_index : automático
+        to_index : automático
+
+        Returns
+        -------
+        float
+            Costo de ir de un nodo a otro.
+
+        '''
         from_node = self.__manager.IndexToNode(from_index)
         to_node = self.__manager.IndexToNode(to_index)
         return self.__data["distance_matrix"][from_node][to_node]
