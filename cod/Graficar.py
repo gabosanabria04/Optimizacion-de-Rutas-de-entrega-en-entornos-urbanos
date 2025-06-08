22c401df7bcbd4aa40f616d4e1fec3dee10fdcf8
Paola María Espinoza Hernández <paola.espinozahernandez@ucr.ac.cr>
2025-06-08 17:59:00 -0600

    feat: documentación

--- cod/Graficar.py
+++ cod/Graficar.py
@@ -1,7 +1,24 @@
 import matplotlib.pyplot as plt
+import osmnx as ox
+import networkx as nx
 
 class Graficar():
-    def __init__(self, mapa, dict_ruta):
+    def __init__(self, mapa, dict_ruta: dict):
+        '''
+        Constructor de la clase Graficar.
+
+        Parameters
+        ----------
+        mapa : Mapa
+            Objeto de la clase Mapa.
+        dict_ruta : dict
+            Diccionario con datos de enrutamiento.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__mapa = mapa.mapa()
         self.__lugares = mapa.lugares
         self.__coordenadas = mapa.coordenadas()
@@ -9,37 +26,139 @@ class Graficar():
 
     @property
     def mapa(self):
+        '''
+        Getter del atributo mapa.
+
+        Returns
+        -------
+        networkx.classes.multidigraph.MultiDiGraph
+            Mapa alrededor de los lugares de interés.
+
+        '''
         return self.__mapa
 
     @mapa.setter
     def mapa(self, new_value):
+        '''
+        Setter del atributo mapa.
+
+        Parameters
+        ----------
+        new_value : networkx.classes.multidigraph.MultiDiGraph
+            Nuevo mapa de los lugares de interés.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__mapa = new_value
 
     @property
     def lugares(self):
+        '''
+        Getter del atributo lugares.
+
+        Returns
+        -------
+        list
+            Lista con los nombres de los lugares de interés.
+
+        '''
         return self.__lugares
 
     @lugares.setter
-    def lugares(self, new_value):
+    def lugares(self, new_value: list):
+        '''
+        Setter del atributo lugares.
+
+        Parameters
+        ----------
+        new_value : list
+            Lista con los nombres de los nuevos lugares de interés.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__lugares = new_value
 
     @property
     def coordenadas(self):
+        '''
+        Getter del atributo coordenadas.
+
+        Returns
+        -------
+        list
+            Lista de coordenadas de los lugares de interés.
+
+        '''
         return self.__coordenadas
 
     @coordenadas.setter
-    def coordenadas(self, new_value):
+    def coordenadas(self, new_value: list):
+        '''
+        Setter del atributo coordenadas.
+
+        Parameters
+        ----------
+        new_value : list
+            Lista con las nuevas coordenadas de los lugares de interés.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__coordenadas = new_value
 
     @property
     def rutas(self):
+        '''
+        Getter del atributo rutas.
+
+        Returns
+        -------
+        list
+            El recorrido de la ruta, en formato lista.
+
+        '''
         return self.__rutas
 
     @rutas.setter
-    def rutas(self, new_value):
+    def rutas(self, new_value: list):
+        '''
+        Setter del atributo rutas.
+
+        Parameters
+        ----------
+        new_value : list
+            Nuevo recorrido de la ruta.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__rutas = new_value
 
-    def graficar_una(self, index):
+    def graficar_una(self, index: int):
+        '''
+        Grafica una parte de la ruta, utilizando el mapa asociado al objeto.
+
+        Parameters
+        ----------
+        index : int
+            Número de recorrido deseado. Empieza en 1.
+
+        Returns
+        -------
+        fig : Figure
+            Gráfico del recorrido indicado.
+
+        '''
         n_origen = self.__rutas[index-1]
         n_destino = self.__rutas[index]
         print(f'va de {self.__lugares[n_origen]} a {self.__lugares[n_destino]}')
