f749165fbb6f87c68f72951011c23f434b89f624
Paola María Espinoza Hernández <paola.espinozahernandez@ucr.ac.cr>
2025-06-08 17:38:02 -0600

    feat: documentación

--- cod/Enrutamiento.py
+++ cod/Enrutamiento.py
@@ -1,16 +1,18 @@
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
 class Enrutamiento():
-    def __init__(self, matriz, depot: int):
+    def __init__(self, matriz, depot: int, matriz_costo = None):
         '''
         Constructor de la clase Enrutamiento
 
         Parameters
         ----------
-        matriz : np.ndarray
+        matriz : numpy.ndarray
             Matriz de distancias entre los lugares.
         depot : int
             Índice del lugar de inicio (y fin).
+        matriz_costo : numpy.ndarray
+            Matriz de costos. Opcional, por defecto, la matriz de distancias.
 
         Returns
         -------
@@ -26,6 +28,10 @@ class Enrutamiento():
             len(self.__data["distance_matrix"]), self.__data["num_vehicles"], self.__data["depot"]
         )
         self.__routing = pywrapcp.RoutingModel(self.__manager)
+        if matriz_costo == None:
+            self.__matriz_costo = matriz
+        else:
+            self.__matriz_costo = matriz_costo
 
     @property
     def data(self):
@@ -117,9 +123,47 @@ class Enrutamiento():
         '''
         self.__routing = new_value
 
+    @property
+    def matriz_costo(self):
+        '''
+        Getter de la matriz de costos.
+
+        Returns
+        -------
+        numpy.ndarray
+            Matriz con los costos de transporte entre cada lugar.
+        '''
+        return self.__matriz_costo
+    
+    @matriz_costo.setter
+    def matriz_costo(self, new_value):
+        '''
+        Setter de la matriz de costos.
+
+        Parameters
+        ----------
+        new_value : numpy.ndarray
+            Nueva matriz de costos de transporte entre cada lugar.
+
+        Returns
+        -------
+        None.
+
+        '''
+        self.__matriz_costo = new_value
+        
+    def __str__(self):
+        return f'''Este es un objeto de Enrutamiento, que utilizará
+                Datos: {self.__data} 
+                Manager: {self.__manager}
+                Routing: {self.__routing}
+                Matriz de costo: {self.__matriz_costo}
+                '''
+
     def distance_callback(self, from_index, to_index):
         '''
         Calcula el costo de ir de un nodo a otro. Los parámetros son propios de ortools.
+        Utiliza la matriz de costos si se proporcionó una.
 
         Parameters
         ----------
@@ -130,13 +174,26 @@ class Enrutamiento():
         -------
         float
             Costo de ir de un nodo a otro.
-
         '''
         from_node = self.__manager.IndexToNode(from_index)
         to_node = self.__manager.IndexToNode(to_index)
-        return self.__data["distance_matrix"][from_node][to_node]
+        return self.__matriz_costo[from_node][to_node]
         
     def enrutar(self):   
+        '''
+        Retorna la ruta más económica, según la matriz de costos.
+
+        Returns
+        -------
+        dict_solucion : dict
+            Diccionario con la distancia objetivo, la distancia del recorrido y la ruta.
+            plan_output : list
+                Recorrido óptimo en formato lista.
+            recorrido_objetivo : str
+                Recorrido objetivo en kilómetros.
+            recorrido_total : str
+                Recorrido total en kilómetros.
+        '''
         transit_callback_index = self.__routing.RegisterTransitCallback(
             lambda from_index, to_index: self.distance_callback(from_index, to_index)
         )
@@ -145,7 +202,6 @@ class Enrutamiento():
         search_parameters = pywrapcp.DefaultRoutingSearchParameters()
         search_parameters.first_solution_strategy = (
             routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
-            # https://developers.google.com/optimization/routing/routing_options?hl=es-419#first_solution_strategy
         )
         solution = self.__routing.SolveWithParameters(search_parameters)
 
@@ -159,10 +215,12 @@ class Enrutamiento():
             index = solution.Value(self.__routing.NextVar(index))
             route_distance += self.__routing.GetArcCostForVehicle(previous_index, index, 0)
         plan_output.append(self.__manager.IndexToNode(index))
+        recorrido_objetivo = f'{solution.ObjectiveValue()} km'
+        recorrido_total = f'{route_distance} km'
         if solution:
             dict_solucion = {
-                "Objetivo": f'{solution.ObjectiveValue()} km',
+                "Objetivo": recorrido_objetivo,
                 "Ruta": plan_output,
-                "Recorrido total": f'{route_distance} km'
+                "Recorrido total": recorrido_total
             }
         return dict_solucion
