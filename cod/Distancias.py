0cc266f90ecdd756e2f616a034a3cb2bdd8a29c5
Paola María Espinoza Hernández <paola.espinozahernandez@ucr.ac.cr>
2025-06-08 18:20:23 -0600

    feat: documentación

--- cod/Distancias.py
+++ cod/Distancias.py
@@ -4,26 +4,107 @@ import numpy as np
 
 class Distancias():
     def __init__(self, objeto_mapa):
+        '''
+        Constructor de la clase Distancias.
+
+        Parameters
+        ----------
+        objeto_mapa : Mapa
+            Objeto de la clase Mapa.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__mapa = objeto_mapa.mapa()
         self.__coordenadas = objeto_mapa.coordenadas()
     
     @property
     def mapa(self):
+        '''
+        Getter del atributo mapa.
+
+        Returns
+        -------
+        networkx.classes.multidigraph.MultiDiGraph
+            Mapa alrededor de los lugares de interés.
+
+        '''
         return self.__mapa
 
     @mapa.setter
     def mapa(self, new_value):
+        '''
+        Setter del atributo mapa.
+
+        Parameters
+        ----------
+        new_value : networkx.classes.multidigraph.MultiDiGraph
+            Nuevo mapa de los lugares de interés.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__mapa = new_value
 
     @property
     def coordenadas(self):
+        '''
+        Getter del atributo coordenadas.
+
+        Returns
+        -------
+        list
+            Lista de coordenadas de los lugares de interés.
+
+        '''
         return self.__coordenadas
 
     @coordenadas.setter
-    def coordenadas(self, new_value):
+    def coordenadas(self, new_value: list):
+        '''
+        Setter del atributo coordenadas.
+
+        Parameters
+        ----------
+        new_value : list
+            Lista de nuevas coordenadas de los lugares de interés.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__coordenadas = new_value
         
+    def __str__(self):
+        '''
+        Método str de la clase Distancias.
+
+        Returns
+        -------
+        str
+            Descripción del objeto y sus atributos.
+
+        '''
+        return f'''Este es un objeto de tipo Distancias con:
+            Objeto Mapa: {self.__objeto_mapa}
+            '''
+        
     def distancias(self):
+        '''
+        Retorna la matriz de distancias entre los lugares de interés, en el mapa.
+
+        Returns
+        -------
+        distance_matrix_km : numpy.ndarray
+            Matriz con las menores distancias entre los lugares de interés.
+            La distancia se calcula de acuerdo al grafo/mapa, es decir, en carretera.
+
+        '''
         nodes = [ox.nearest_nodes(self.__mapa, lon, lat) for lat, lon in self.__coordenadas]
         
         n = len(nodes)
